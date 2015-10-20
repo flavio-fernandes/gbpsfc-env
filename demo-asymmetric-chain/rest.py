722552f8b464c7c55df1bef26ffef2fd26d6d910
Flavio Fernandes <ffernand@redhat.com>
2015-10-19 22:39:26 -0400

    update rest.py

--- demo-asymmetric-chain/rest.py
+++ demo-asymmetric-chain/rest.py
@@ -660,6 +660,68 @@ def get_endpoint_data():
 def get_endpoint_uri():
     return "/restconf/operations/endpoint:register-endpoint"
 
+def get_ietf_acl_uri():
+    return "/restconf/config/ietf-access-control-list:access-lists"
+
+def get_ietf_acl_data():
+    return {
+        "access-lists": {
+            "acl": [
+                {
+                    "acl-name": "http-acl",
+                    "access-list-entries": {
+                        "ace": [
+                            {
+                                "rule-name": "http-rule",
+                                "matches": {
+                                    "destination-port-range": {
+                                        "lower-port": "80",
+                                        "upper-port": "80"
+                                        }
+                                    },
+                                "actions": {
+                                    "permit": "true"                                
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+def get_classifier_uri():
+    return "/restconf/config/netvirt-sfc-classifier:classifiers"
+
+def get_classifier_data():
+    return {
+        "classifiers": {
+            "classifier": [
+                {
+                    "name": "http-classifier",
+                    "acl": "http-acl",
+                    "sffs": {
+                        "sff": [
+                            {
+                                "name": "sff1"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+def get_netvirt_sfc_uri():
+    return "/restconf/config/netvirt-sfc:sfc/"
+
+def get_netvirt_sfc_data():
+    return {
+        "sfc": {
+            "name": "sfc1"
+            }
+        }
+
 if __name__ == "__main__":
     # Launch main menu
 
@@ -677,14 +739,26 @@ if __name__ == "__main__":
     put(controller, DEFAULT_PORT, get_service_functions_uri(), get_service_functions_data(), True)
     print "sending service function forwarders"
     put(controller, DEFAULT_PORT, get_service_function_forwarders_uri(), get_service_function_forwarders_data(), True)
-    print "sending service function chains"
-    put(controller, DEFAULT_PORT, get_service_function_chains_uri(), get_service_function_chains_data(), True)
-    print "sending service function paths"
-    put(controller, DEFAULT_PORT, get_service_function_paths_uri(), get_service_function_paths_data(), True)
-    ## print "sending tunnel -- SKIPPED"
+
+    # print "sending service function chains"
+    # put(controller, DEFAULT_PORT, get_service_function_chains_uri(), get_service_function_chains_data(), True)
+    # print "sending service function paths"
+    # put(controller, DEFAULT_PORT, get_service_function_paths_uri(), get_service_function_paths_data(), True)
+
+    print "sending netvirt-sfc"
+    put(controller, DEFAULT_PORT, get_netvirt_sfc_uri(), get_netvirt_sfc_data(), True)
+    print "sending ietf-acl"
+    put(controller, DEFAULT_PORT, get_ietf_acl_uri(), get_ietf_acl_data(), True)
+    print "sending classifier"
+    put(controller, DEFAULT_PORT, get_classifier_uri(), get_classifier_data(), True)
+
+
+    # print "sending tunnel -- SKIPPED"
     ## put(controller, DEFAULT_PORT, get_tunnel_uri(), get_tunnel_data(), True)
-    print "sending tenant -- SKIPPED"
+    # print "sending tenant -- SKIPPED"
     ## put(controller, DEFAULT_PORT, get_tenant_uri(), get_tenant_data(),True)
-    print "registering endpoints -- SKIPPED"
+    # print "registering endpoints -- SKIPPED"
     ## for endpoint in get_endpoint_data():
     ##    post(controller, DEFAULT_PORT, get_endpoint_uri(),endpoint,True)
+
+
